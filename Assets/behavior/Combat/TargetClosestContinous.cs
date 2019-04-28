0abad2db85697390fbccc56984f5e28cd498f5a1
Ferdinand Mathia <FerdinandPeer.Mathia@HS-Augsburg.DE>
2019-04-28 12:24:40 +0200

    improved targeting

--- Assets/behavior/Combat/TargetClosestContinous.cs
+++ Assets/behavior/Combat/TargetClosestContinous.cs
@@ -8,30 +8,19 @@ namespace behavior.Combat
     {
         public int checkPeriod = 10;
         private int ticksToNextCheck = 1;
-        public GameObject subSet;
         private HasTarget targeter;
+        public string targetTag;
 
 
         // Start is called before the first frame update
         void Update()
         {
-            if (subSet == null)
-            {
-                Debug.LogError("shit Target closest has started to soon");
-                return;
-            }
-
             ticksToNextCheck -= 1;
             if (ticksToNextCheck <= 0)
             {
                 ticksToNextCheck = checkPeriod;
-
-                var potential_targets = subSet.GetComponentsInChildren<Damagable>();
-//                if (potential_targets.Length == 0 )
-//                {}
-                var target = potential_targets
-                    .OrderByDescending(p => (p.transform.position - this.transform.position).magnitude)
-                    .FirstOrDefault(); //TODO this does not work right, maybe try spheerecasting
+                
+                var target = findClosestEnemy();
                 if (target != null)
                 {
                     targeter.target = target.transform;
@@ -46,6 +35,26 @@ namespace behavior.Combat
         private void Awake()
         {
             targeter = GetComponent<HasTarget>();
+        }
+        
+        public GameObject findClosestEnemy()
+        {
+            GameObject[] gos;
+            gos = GameObject.FindGameObjectsWithTag(targetTag);
+            GameObject closest = null;
+            float distance = Mathf.Infinity;
+            Vector3 position = transform.position;
+            foreach (GameObject go in gos)
+            {
+                Vector3 diff = go.transform.position - position;
+                float curDistance = diff.sqrMagnitude;
+                if (curDistance < distance)
+                {
+                    closest = go;
+                    distance = curDistance;
+                }
+            }
+            return closest;
         }
     }
 }
